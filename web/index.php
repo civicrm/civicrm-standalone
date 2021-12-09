f4cd8caf655738be07878bb06a99b4c8404ed66e
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-12-09 11:49:00 -0500

    index.php: load CSS/JS, and more fixes around the invoke call

--- web/index.php
+++ web/index.php
@@ -4,22 +4,22 @@ require_once '../vendor/autoload.php';
 require_once '../civicrm.settings.php';
 
 function invoke() {
-  $session = CRM_Core_Session::singleton();
-  $config = CRM_Core_Config::singleton();
+  if (!empty($_SERVER['REQUEST_URI'])) {
+    // Add CSS, JS, etc. that is required for this page.
+    \CRM_Core_Resources::singleton()->addCoreResources();
 
-  $urlVar = $config->userFrameworkURLVar;
-  
-  if (!empty($_GET[$urlVar])) {
-    print CRM_Core_Invoke::invoke(explode('/', $_GET[$urlVar]));
-  }
-  elseif (!empty($_SERVER['REQUEST_URI'])) {
     $parts = explode('?', $_SERVER['REQUEST_URI']);
     $args = explode('/', $parts[0]);
     // Remove empty values
     $args = array_values(array_filter($args));
+    // Set this for compatibility
+    $_GET['q'] = implode('/', $args);
+    // And finally render the page
     print CRM_Core_Invoke::invoke($args);
   }
   else {
+    $config = CRM_Core_Config::singleton();
+    $urlVar = $config->userFrameworkURLVar;
     print CRM_Core_Invoke::invoke(explode('/', $_GET[$urlVar]));
   }
 }
