879b60867c9aa59523c8083bae3625a683d7b0d5
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-12-17 09:23:44 -0500

    Partial civicrm-setup support

--- web/index.php
+++ web/index.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php';
-require_once '../civicrm.settings.php';
-
 function invoke() {
   // Redirect to the dashboard for discoverability
   // but in the future we may want a login page or something else.
@@ -34,4 +31,35 @@ function invoke() {
   }
 }
 
-invoke();
+if (file_exists('../civicrm.settings.php')) {
+  require_once '../vendor/autoload.php';
+  require_once '../civicrm.settings.php';
+  invoke();
+}
+else {
+  $coreUrl = '/assets/civicrm/core';
+  $root = dirname(__DIR__);
+  $civiroot = implode(DIRECTORY_SEPARATOR, [$root, 'vendor', 'civicrm', 'civicrm-core']);
+  $classLoader = implode(DIRECTORY_SEPARATOR, [$civiroot, 'CRM', 'Core', 'ClassLoader.php']);
+
+  if (file_exists($classLoader)) {
+    require_once $classLoader;
+    CRM_Core_ClassLoader::singleton()->register();
+    \Civi\Setup::assertProtocolCompatibility(1.0);
+    \Civi\Setup::init([
+      // This is just enough information to get going. Drupal.civi-setup.php does more scanning.
+      'cms' => 'Standalone',
+      'webroot' => $root,
+      'srcPath' => $civiroot,
+    ]);
+    $ctrl = \Civi\Setup::instance()->createController()->getCtrl();
+    $ctrl->setUrls([
+      'ctrl' => 'civicrm', // @todo this had url('civicrm')
+      'res' => $coreUrl . '/setup/res/',
+      'jquery.js' => $coreUrl . '/bower_components/jquery/dist/jquery.min.js',
+      'font-awesome.css' => $coreUrl . '/bower_components/font-awesome/css/font-awesome.min.css',
+    ]);
+    \Civi\Setup\BasicRunner::run($ctrl);
+    exit();
+  }
+}
